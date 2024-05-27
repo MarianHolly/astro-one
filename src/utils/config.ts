33d8258b5b2266340c66b3ef20c84c300925e05c
Stupel <hellomariandev@gmail.com>
2024-05-27 11:32:27 +0200

    routing, pages and components

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,12 @@
 export const SiteConfig = {
-    site_name: 'Astro One',
-    title: 'Astro One | Astro with Tailwind Css',
-    description: 'Astro project to learn Astro framework'
-}
+  site_name: "Astro One",
+  title: "Astro One | Astro with Tailwind Css",
+  description: "Astro project to learn Astro framework",
+};
+
+export const navigation = [
+  { title: "Home", path: "/" },
+  { title: "About", path: "/about" },
+  { title: "Blog", path: "/posts" },
+  { title: "Contact", path: "/contact" },
+];
